fc2c6968aa4d5c8aee138cbc57850bad07141b17
Camilo <yeisongamba97@gmail.com>
2025-10-28 21:43:47 -0500

    renew

--- node_modules/react-reconciler/constants.js
+++ node_modules/react-reconciler/constants.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./cjs/react-reconciler-constants.production.js');
+  module.exports = require('./cjs/react-reconciler-constants.production.min.js');
 } else {
   module.exports = require('./cjs/react-reconciler-constants.development.js');
 }
