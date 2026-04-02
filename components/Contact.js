7e2b42c5dab2f9879a4f799ed6993e8d3e762187
Camilo <yeisongamba97@gmail.com>
2026-04-02 18:28:10 -0500

    feat: integrate authentic social media links

--- components/Contact.js
+++ components/Contact.js
@@ -20,10 +20,10 @@ const Contact = () => {
       <div className={styles.bottomBar}>
         <div className="container" style={{ display: 'flex', justifyContent: 'center', fontSize: '0.65rem' }}>
           <div style={{ display: 'flex', gap: 'var(--spacing-4)' }}>
-            <a href="#">LINKEDIN</a>
-            <a href="#">INSTAGRAM</a>
-            <a href="#">BEHANCE</a>
-            <a href="#">DRIBBBLE</a>
+            <a href="https://www.linkedin.com/in/yeison-camilo-gamba-gonzalez-10776268/" target="_blank" rel="noopener noreferrer">LINKEDIN</a>
+            <a href="https://www.instagram.com/creat3di/?utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGnaKLxk9GhyICBoxK1TLF1LDblaqv3XNfI1xuma9TojDAK4hAQsN2FHjO4sS8_aem_D3YRuq-M3BAyAZHd4qjejw" target="_blank" rel="noopener noreferrer">INSTAGRAM</a>
+            <a href="https://www.behance.net/yeisongamba97" target="_blank" rel="noopener noreferrer">BEHANCE</a>
+            <a href="https://dribbble.com/camilog97" target="_blank" rel="noopener noreferrer">DRIBBBLE</a>
           </div>
         </div>
       </div>
