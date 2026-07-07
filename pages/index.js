b5c936cd184a38695e9ba093cb92717242f778d1
Camilo <yeisongamba97@gmail.com>
2026-07-06 19:17:32 -0500

    style: reorder sections, move Roles after Experience and before Certifications

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,9 @@ export default function HomePage() {
   return (
     <Layout>
       <Hero />
-      <Roles />
       <Portfolio />
       <Experience />
+      <Roles />
       <Certifications />
       <Contact />
     </Layout>
